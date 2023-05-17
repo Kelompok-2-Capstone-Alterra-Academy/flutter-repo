68208503d00e5f10edcb7779657f6633a9254e17
Rizky P <rizky.putra168@gmail.com>
2023-05-17 17:44:48 +0700

    Adding Folder Structure and Dependencies

--- lib/utils/color/color.dart
+++ lib/utils/color/color.dart
@@ -7,5 +7,7 @@ const Color warningColor = Color(0xFFFF9800);
 const Color subjectColor = Color(0xFFFFE0B2);
 const Color lightBlueColor = Color(0xFFF0FAFF);
 const Color lightGreenColor = Color(0xFFE1EFE1);
+const Color searchBarColor = Color(0xFFF5F5F5);
+const Color searchBarTextColor = Color(0xFF9E9E9E);
 const Color blackColor = Colors.black;
 const Color whiteColor = Colors.white;
