4ec25102c9987624d7a3c9ec26f34f855d938780
Rizky P <rizky.putra168@gmail.com>
2023-05-17 16:48:30 +0700

    Adding Folder Structure and Dependencies

---
+++ lib/utils/color/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF2196F3);
+const Color secondaryColor = Color(0xFF4161FF);
+const Color successColor = Color(0xFF4CAF50);
+const Color warningColor = Color(0xFFFF9800);
+const Color subjectColor = Color(0xFFFFE0B2);
+const Color lightBlueColor = Color(0xFFF0FAFF);
+const Color lightGreenColor = Color(0xFFE1EFE1);
+const Color blackColor = Colors.black;
+const Color whiteColor = Colors.white;
