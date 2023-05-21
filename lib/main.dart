df50c391ce27f603e37ab2ba297d00aa340f0ddb
dwibagus154 <dwibagus154@gmail.com>
2023-05-21 09:45:54 +0700

    feat: add feature forgot password

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:staredu/views/screen/auth/forgot_password/main/forgot_password_screen.dart';
 import 'package:staredu/views/screen/auth/login/login_screen.dart';
 import 'package:staredu/views/screen/home/home_screen.dart';
 import 'package:staredu/views/screen/splash_welcome/splash_welcome_screen.dart';
@@ -13,17 +14,19 @@ class StarEdu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rent Car',
+      title: 'StarEdu',
       debugShowCheckedModeBanner: false,
       // add primary color
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      initialRoute: LoginScreen.routeName,
+      initialRoute: ForgotPasswordScreen.routeName,
       routes: {
         SplashWelcomeScreen.routeName: (context) => const SplashWelcomeScreen(),
         HomeScreen.routeName: (context) => const HomeScreen(),
         LoginScreen.routeName: (context) => const LoginScreen(),
+        ForgotPasswordScreen.routeName: (context) =>
+            const ForgotPasswordScreen(),
       },
     );
   }
