178c7e9a4a5f6306bafd7b0ad9b4f16ec713fa40
ahmadabidb <ahmadabidb909@gmail.com>
2023-05-24 09:12:29 +0700

    feat: post feed user activity

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:staredu/views/screen/auth/login/login_screen.dart';
 import 'package:staredu/views/screen/home/home_screen.dart';
+import 'package:staredu/views/screen/post_feed/post_feed_screen.dart';
 import 'package:staredu/views/screen/splash_welcome/splash_welcome_screen.dart';
 
 void main() {
@@ -19,11 +20,12 @@ class StarEdu extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      initialRoute: LoginScreen.routeName,
+      initialRoute: PostFeedScreen.routeName,
       routes: {
         SplashWelcomeScreen.routeName: (context) => const SplashWelcomeScreen(),
         HomeScreen.routeName: (context) => const HomeScreen(),
         LoginScreen.routeName: (context) => const LoginScreen(),
+        PostFeedScreen.routeName: (context) => const PostFeedScreen()
       },
     );
   }
---
+++ lib/views/screen/post_feed/post_feed_screen.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../../models/post_feed_model.dart';
+import '../../../utils/color/color.dart';
+import '../../../utils/constant/list_post_feed.dart';
+
+class PostFeedScreen extends StatefulWidget {
+  const PostFeedScreen({super.key});
+  static const routeName = '/postfeed';
+
+  @override
+  State<PostFeedScreen> createState() => _PostFeedScreenState();
+}
+
+class _PostFeedScreenState extends State<PostFeedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    late List<PostFeedModel> postFeeds = postFeedsData;
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(children: [
+          const SizedBox(height: 55),
+          Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const SizedBox(width: 55),
+              Text(
+                "Aktifitas Pengguna",
+                style: GoogleFonts.poppins(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                backgroundColor: searchBarTextColor,
+                child: Icon(
+                  Icons.person,
+                  color: whiteColor,
+                ),
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: Container(
+                  height: 42,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    color: whiteColor,
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Ceritakan pengalamanmu",
+                        style: GoogleFonts.poppins(
+                          color: searchBarTextColor,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: postFeeds.length,
+              itemBuilder: (context, index) {
+                PostFeedModel postFeed = postFeeds[index];
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              backgroundColor: searchBarTextColor,
+                              child: Icon(
+                                Icons.person,
+                                color: whiteColor,
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  postFeed.name ?? '',
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  postFeed.time ?? '',
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 10,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          postFeed.comment ?? '',
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            IconButton(
+                              icon: const Icon(
+                                Icons.thumb_up,
+                                color: Colors.blue,
+                              ),
+                              onPressed: () {},
+                            ),
+                            Text(
+                              postFeed.like ?? '',
+                              style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 11,
+                              ),
+                            ),
+                            const SizedBox(width: 15),
+                            IconButton(
+                              icon: const Icon(Icons.comment_outlined),
+                              onPressed: () {},
+                            ),
+                            Text(
+                              postFeed.reply ?? '',
+                              style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 11,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
