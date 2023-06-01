a1f84e0f9dae1415f19eebdf2877e93b23a01cfe
dwibagus154 <dwibagus154@gmail.com>
2023-06-02 00:09:19 +0700

    feat: integrate api to verify otp

--- lib/views/screen/auth/forgot_password/forgot_password_view_model.dart
+++ lib/views/screen/auth/forgot_password/forgot_password_view_model.dart
@@ -5,8 +5,10 @@ import 'package:staredu/utils/state/my_state.dart';
 class ForgotPasswordViewModel extends ChangeNotifier {
   dynamic response;
   MyState _stateForgotPassword = MyState.initial;
+  MyState _stateOtp = MyState.initial;
 
   MyState get stateForgotPassword => _stateForgotPassword;
+  MyState get stateOtp => _stateOtp;
 
   Future<String> forgotPassword(String email) async {
     response = await AuthAPI.forgotPassword(email);
@@ -25,8 +27,30 @@ class ForgotPasswordViewModel extends ChangeNotifier {
     }
   }
 
+  Future<String> verifyOtp(String email, String otp) async {
+    response = await AuthAPI.verifyOtp(email, otp);
+
+    if (response == null) {
+      setStateOtp(MyState.failed);
+      return 'Verify OTP Failed';
+    }
+
+    if (response['status_code'] == 200) {
+      setStateOtp(MyState.success);
+      return 'success';
+    } else {
+      setStateOtp(MyState.failed);
+      return response['message'];
+    }
+  }
+
   void setStateForgotPassword(MyState state) {
     _stateForgotPassword = state;
+    notifyListeners();
+  }
+
+  void setStateOtp(MyState state) {
+    _stateOtp = state;
     notifyListeners();
   }
 }
