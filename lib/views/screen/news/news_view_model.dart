1b52adc29dc5280dc602323754ca9af37ccfbc64
ahmadabidb <ahmadabidb909@gmail.com>
2023-06-19 19:31:21 +0700

    update news feature

---
+++ lib/views/screen/news/news_view_model.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:staredu/models/service/news_api.dart';
+import '../../../models/news_model.dart';
+import '../../../utils/state/my_state.dart';
+
+class NewsViewModel extends ChangeNotifier {
+  List<NewsModel> _newsList = [];
+  List<NewsModel> get newsList => _newsList;
+
+  final NewsAPI newsAPI = NewsAPI();
+
+  MyState myState = MyState.initial;
+
+  Future getAllNews() async {
+    try {
+      myState = MyState.loading;
+      notifyListeners();
+
+      _newsList = await newsAPI.getAllNews();
+
+      myState = MyState.success;
+      notifyListeners();
+    } catch (e) {
+      if (e is DioError) {
+        e.response?.statusCode;
+      }
+
+      myState = MyState.failed;
+      notifyListeners();
+    }
+  }
+}
